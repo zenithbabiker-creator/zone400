8059a1a0a4463607cf196814e8e0b1a441b8e650
zenithbabiker-creator <zenithbabiker@gmail.com>
2026-07-14 00:31:19 +0200

    Update build.gradle.kts

--- LandscapeDesignApp/LandscapeDesignApp/build.gradle.kts
+++ LandscapeDesignApp/LandscapeDesignApp/build.gradle.kts
@@ -1,12 +1,5 @@
-dependencies {
-    // العودة لإصدار Sceneview القديم المتوافق مع كودك
-    implementation("io.github.sceneview:arsceneview:0.10.0")
-
-    // العودة لإصدار Material 1.1.2 لتجنب أخطاء ExposedDropdownMenu
-    implementation("androidx.compose.material3:material3:1.1.2")
-    
-    // باقي المكتبات كما هي...
-    implementation("com.google.ar:core:1.44.0")
-    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
-    // ... باقي الـ dependencies
+// الملف الموجود في المجلد الرئيسي (Root)
+plugins {
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
 }
