44920c8f65217f7920d3e3891c68dfc5b0988096
zenithbabiker-creator <zenithbabiker@gmail.com>
2026-07-13 14:50:44 +0200

    Update build.gradle.kts

--- LandscapeDesignApp/LandscapeDesignApp/build.gradle.kts
+++ LandscapeDesignApp/LandscapeDesignApp/build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file
 plugins {
     id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
 }
