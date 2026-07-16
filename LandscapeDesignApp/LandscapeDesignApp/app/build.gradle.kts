52e78abe5c0260075d02c6857f71a4f4270fa2c3
zenithbabiker-creator <zenithbabiker@gmail.com>
2026-07-16 17:58:09 +0200

    Update build.gradle.kts

--- LandscapeDesignApp/LandscapeDesignApp/app/build.gradle.kts
+++ LandscapeDesignApp/LandscapeDesignApp/app/build.gradle.kts
@@ -1,8 +1,7 @@
-// الملف الموجود داخل مجلد (app)
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.parcelize") // تأكد من استخدام هذا الاسم
+    id("org.jetbrains.kotlin.plugin.parcelize")
 }
 
 android {
@@ -16,7 +15,7 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-    
+
     buildFeatures {
         compose = true
     }
@@ -36,13 +35,9 @@ android {
 }
 
 dependencies {
-    // ARCore
     implementation("com.google.ar:core:1.44.0")
-    
-    // Sceneview المتوافق مع كودك
     implementation("io.github.sceneview:arsceneview:0.10.0")
-
-    // Jetpack Compose
+    
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -54,9 +49,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.2")
     implementation("androidx.navigation:navigation-compose:2.7.7")
-
-    // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
-
+    
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
